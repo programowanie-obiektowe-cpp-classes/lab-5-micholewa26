7be5f64862e1aa56fc1f6ed7d999395da885fc5e
micholewa26 <michallejwoda@biskupiak.pl>
2025-11-27 10:07:27 +0100

    [zal

--- foo.hpp
+++ foo.hpp
@@ -7,6 +7,25 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+   
+    for (auto it = people.begin(); it != people.end(); ++it)
+    {
+        it->birthday();
+        
+    }
+    
+    std::vector< char > result;
+    for (auto rit = people.rbegin(); rit!= people.rend(); rit++) 
+        {
+            if (rit->isMonster()) {
+            
+                result.push_back('n');
+            } 
+            else {
+                result.push_back('y');
+                }
+    }
+
+
+    return result;
 }
